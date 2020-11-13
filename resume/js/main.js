8392c907d5932c5cd1595230b421b56535c13896
Kha Nguyen <khanguyen1000@gmail.com>
2020-11-13 22:17:02 +0700

    update

--- resume/js/main.js
+++ resume/js/main.js
@@ -26,3 +26,22 @@ fillData(
 
 
 
+$(document).ready(function () {
+    let $btns = $('#duAn_portfolio .btn-group button');
+    $btns.click(function (e) {
+        $('#duAn_portfolio .btn-group button').removeClass('active');
+        e.target.classList.add("active");
+        let selector = $(e.target).attr('data-filter');
+        $('#duAn_portfolio .grid').isotope({
+            filter: selector,
+            percentPosition: true,
+            itemSelector: '.element-item',
+        });
+        return false;
+    });
+})
+
+$('#duAn_portfolio .grid').isotope({
+    percentPosition: true,
+    filter: '*',
+});
